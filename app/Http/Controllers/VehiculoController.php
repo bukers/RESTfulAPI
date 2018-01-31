fcebd57d8d88f283d8861e0e2e9fa4acf3ad155f
huesos <bukers12@gmail.com>
2018-01-31 13:26:44 -0600

    Actualizacion de route
    se optimizo las rutas que se tenia en el inicio del proyecto

--- app/Http/Controllers/VehiculoController.php
+++ app/Http/Controllers/VehiculoController.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
-use App\Vehiculo;
-
 class VehiculoController extends Controller {
 
 	/**
@@ -16,7 +14,27 @@ class VehiculoController extends Controller {
 	 */
 	public function index()
 	{
-		return response()->json(['datos' => Vehiculo::all()],200);
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
 	}
 
 	/**
@@ -27,14 +45,40 @@ class VehiculoController extends Controller {
 	 */
 	public function show($id)
 	{
-		$vehiculo = Vehiculo::find($id);
+		//
+	}
 
-		if(!$vehiculo)
-		{
-			return response()->json(['mensaje' => 'No se encuentra este vehiculo', 'codigo' => 404],404);
-		}
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
 
-		return response()->json(['datos' => $vehiculo],200);
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
 	}
 
 }
