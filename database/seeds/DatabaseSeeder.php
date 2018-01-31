a027ae08dba0b1e21b508494a88c5bdb96430ffa
huesos <bukers12@gmail.com>
2018-01-31 12:37:00 -0600

    Se crean los Seeder y las rutas REST
    -aca vamos acrear la clase para crear datos falsos en la base de datos
    -se generan las rutas REST para la api

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,18 +3,26 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+
 class DatabaseSeeder extends Seeder {
 
 	/**
 	 * Run the database seeds.
-	 *
+	 * 
 	 * @return void
 	 */
 	public function run()
 	{
-		Model::unguard();
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+		
+		$this->call('FabricanteSeeder');
+		$this->call('VehiculoSeeder');
 
-		// $this->call('UserTableSeeder');
+		User::truncate();
+		DB::table('oauth_clients')->truncate();
+		$this->call('UserSeeder');
+		$this->call('Oauth2Seeder');
 	}
 
 }
